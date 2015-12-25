d6e8d8e5ce5ecdbb984ef9bef6ddf814b79002d9
David Barrell <david_barrell@bigpond.com>
2015-12-26 02:29:47 +1100

    Initiated auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
+  before_action :require_user, only: [:index, :show]
+
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,15 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
   get 'welcome' => 'pages#home'
   root 'pages#home'
+
+  get 'signup'  => 'users#new'
+  resources :users
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 end
